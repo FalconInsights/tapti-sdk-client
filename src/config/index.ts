d82146cc0b35b4aff5e0c15476ee7f15bd4c3dd6
Areeb ur Rub <areeburrub@gmail.com>
2024-11-25 13:35:10 +0530

    fix: oauth gatway url

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,23 +1,23 @@
 export class SDKConfig {
-    private static instance: SDKConfig;
-    private static accessGatewayBaseUrl: string = "http://localhost:3002";
+  private static instance: SDKConfig;
+  private static accessGatewayBaseUrl: string = 'https://oauth.tapti.ai';
 
-    private constructor() {
-        // Private constructor to prevent direct instantiation
-    }
+  private constructor() {
+    // Private constructor to prevent direct instantiation
+  }
 
-    public static getInstance(): SDKConfig {
-        if (!SDKConfig.instance) {
-            SDKConfig.instance = new SDKConfig();
-        }
-        return SDKConfig.instance;
+  public static getInstance(): SDKConfig {
+    if (!SDKConfig.instance) {
+      SDKConfig.instance = new SDKConfig();
     }
+    return SDKConfig.instance;
+  }
 
-    public static get accessGatewayBaseURL(): string {
-        return SDKConfig.accessGatewayBaseUrl;
-    }
+  public static get accessGatewayBaseURL(): string {
+    return SDKConfig.accessGatewayBaseUrl;
+  }
 
-    public static set accessGatewayBaseURL(url: string) {
-        SDKConfig.accessGatewayBaseUrl = url;
-    }
+  public static set accessGatewayBaseURL(url: string) {
+    SDKConfig.accessGatewayBaseUrl = url;
+  }
 }
